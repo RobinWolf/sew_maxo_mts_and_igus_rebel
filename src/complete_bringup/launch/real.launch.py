81a8d2c7524cce99469038ada777b102c02f756c
Robin Wolf <woro1021@h-ka.de>
2024-05-26 10:47:33 +0200

    delete unused irc packages (only recommendet for real hardware)

---
+++ src/complete_bringup/launch/real.launch.py
@@ -0,0 +1,4 @@
+## TODO when real hardware is available
+
+##just connect to the real hardware instead of gazebo hardware plugins --> use launch arguments and urdf parameters
+##additional igus packages needed: irc_ros_bringup, irc_ros_controllers, irc_ros_hardware, irc_ros_msgs
--- src/irc_ros_msgs/README.md
+++
@@ -1,3 +0,0 @@
-# iRC ROS Messages
-
-This package contains the message and service definitions for exchanging module, dio and gripper information and commands.
