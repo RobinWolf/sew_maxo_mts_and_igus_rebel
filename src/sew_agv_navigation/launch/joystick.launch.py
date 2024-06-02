9d21d1b4b4b80ed0035f4b0a246be6d90687424b
Robin Wolf <woro1021@h-ka.de>
2024-06-02 10:58:51 +0200

    add launch arguments for wether remap topic to diffbot base controller ns os shortcut twistmux

--- src/sew_agv_navigation/launch/joystick.launch.py
+++ src/sew_agv_navigation/launch/joystick.launch.py
@@ -3,6 +3,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 
 import os
@@ -15,10 +16,30 @@ def generate_launch_description():
     #declare launch arguments
     declared_arguments = []
     declared_arguments.append(
-    DeclareLaunchArgument('use_sim_time', default_value='false',
-                            description='Set to "true" if you want to use the joystick with gazebo, set to "fasle" if you use real hardware.'))
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Set to "true" if you want to use the joystick with gazebo, set to "fasle" if you use real hardware.'
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "generate_ros2_control_tag",
+            default_value='false',
+            description="launch hardware drivers for real hardware, mock hardware or gazebo (recommendet to use)",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "standalone_gazebo",
+            default_value='true',
+            description="add the robot description to gazebo with a simpler approach, using a diff_drive and lidar plugin (NOT recommendet to use, only for testing purposes without ros2 control)",
+        )
+    )
     
     use_sim_time = LaunchConfiguration('use_sim_time')   
+    standalone_gazebo = LaunchConfiguration("standalone_gazebo")
+    generate_ros2_control_tag = LaunchConfiguration('generate_ros2_control_tag')
     
     #define nodes to launch
     joy_params = os.path.join(get_package_share_directory(navigation_package),'config', 'joystick', 'joystick.yaml')
@@ -32,11 +53,22 @@ def generate_launch_description():
     )
 
     twistmux_params = os.path.join(get_package_share_directory(navigation_package),'config', 'joystick', 'twistmux.yaml')
+
     twistmux_node = Node(
         package='twist_mux',
         executable='twist_mux',
-        parameters=[twistmux_params,{use_sim_time}],  # just needs to be set up, because topic remapping in gazebo without ros2_control is not possible
-        remappings=[('/cmd_vel_out', '/cmd_vel')]   # until ros2_control is available just shortcut the twistmux node --> controller is not prioritized!
+        parameters=[twistmux_params,{use_sim_time}], 
+        remappings=[('/cmd_vel_out', '/cmd_vel')], # until ros2_control is available just shortcut the twistmux node --> controller is not prioritized!
+        condition=IfCondition(standalone_gazebo)
+   
+    )
+
+    twistmux_node_control = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        parameters=[twistmux_params,{use_sim_time}],  
+        remappings=[('/cmd_vel_out', '/diffbot_base_controller/cmd_vel_unstamped')],
+        condition=IfCondition(generate_ros2_control_tag)
     )
 
     joy_node = Node(
@@ -47,5 +79,5 @@ def generate_launch_description():
         parameters=[{'dev': '/dev/input/js0'}]  # Specify the device file for your joystick
     )
 
-    nodes_to_start = [teleop_joy_node, joy_node, twistmux_node]
+    nodes_to_start = [teleop_joy_node, joy_node, twistmux_node, twistmux_node_control]
     return LaunchDescription(declared_arguments + nodes_to_start)
